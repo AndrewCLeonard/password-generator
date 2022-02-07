2669d7b9c812f1f186d3965b80bbe5cde7b07060
Andrew Leonard <AndrewCLeonard@fastmail.com>
2022-02-06 22:57:15 -0800

    setup and working on modal

---
+++ Develop/script.js
@@ -0,0 +1,55 @@
+// Get the modal
+var modal = document.getElementById("modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+	modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+	modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+};
+
+// ====================================================================================================
+// SLIDER START
+// ====================================================================================================
+
+var slider = document.getElementById("silderRange");
+var output = document.getElementById("sliderValue");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+	output.innerHTML = this.value;
+};
+// ====================================================================================================
+// SLIDER END
+// ====================================================================================================
+
+// ------ starter code -----------------------------------------------------------------------------------------
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+	var password = generatePassword();
+	var passwordText = document.querySelector("#password");
+
+	passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
---
+++ README.md
@@ -0,0 +1,10 @@
+# Password Generator Starter Code
+
+credit:
+W3 Schools 
+
+Modals: 
+https://www.w3schools.com/howto/howto_css_modals.asp
+
+Range Sliders:
+https://www.w3schools.com/howto/howto_js_rangeslider.asp
