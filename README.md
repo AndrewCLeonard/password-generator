b75fd1ce5d553af11cade0201d69c2828947f499
Andrew Leonard <AndrewCLeonard@fastmail.com>
2022-08-04 20:12:36 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
+# User Story
+
+## AS AN employee with access to sensitive data
+
+-   I WANT to randomly generate a password that meets certain criteria
+-   SO THAT I can create a strong password that provides greater security
+
+## Acceptance Criteria
+
+GIVEN I need a new, secure password
+
+| WHEN...                                              | THEN...                                                                                      |
+| ---------------------------------------------------- | -------------------------------------------------------------------------------------------- |
+| I click the button to generate a password            | I am presented with a series of prompts for password criteria                                |
+| prompted for password criteria                       | I select which criteria to include in the password                                           |
+| prompted for the length of the password              | I choose a length of at least 8 characters and no more than 128 characters                   |
+| asked for character types to include in the password | I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters |
+| I answer each prompt                                 | my input should be validated and at least one character type should be selected              |
+| all prompts are answered                             | a password is generated that matches the selected criteria                                   |
+| the password is generated                            | THEN the password is either displayed in an alert or written to the page                     |
+
 # Password Generator Starter Code
 
 credit:
-W3 Schools 
+W3 Schools
 
-Modals: 
+Modals:
 https://www.w3schools.com/howto/howto_css_modals.asp
 
 Range Sliders:
--- script.js
+++ script.js
@@ -1,71 +1,17 @@
-// Get the modal
-var modal = document.getElementById("modal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("btn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function () {
-	modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-	modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-	if (event.target == modal) {
-		modal.style.display = "none";
-	}
-};
-
-// ====================================================================================================
-// SLIDER START
-// ====================================================================================================
-
-var slider = document.getElementById("sliderRange");
-var output = document.getElementById("sliderValue");
-output.innerHTML = slider.value;
-
-slider.oninput = function () {
-	output.innerHTML = this.value;
-};
-// ====================================================================================================
-// SLIDER END
-// ====================================================================================================
-const newPassword = document.querySelector("#createPassword");
-newPassword.addEventListener("click", generatePassword);
-let generatePassword = () => {
-	var formData = document.querySelector("#")
-};
-/*
-
-when "Generate Password" button is clicked:
--	the slider value 
--	any of the checkboxes that are true
-
-are passed to the generate password function
-
-to start, I should see if I can console.log the input from the form. 
-
-*/
-
-// ------ starter code -----------------------------------------------------------------------------------------
+// Assignment code here
+function generatePassword {
+	
+}
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-	var password = generatePassword();
-	var passwordText = document.querySelector("#password");
+	const password = generatePassword();
+	const passwordText = document.querySelector("#password");
 
 	passwordText.value = password;
 }
 
 // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
